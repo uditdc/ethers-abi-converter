68b92a8629c72108334248845e5fd94030223251
Udit <udit@eskaytech.com>
2022-12-30 01:37:48 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Ethers ABI Converter
+===
+
+A simple deno utility to convert abi formats via HTTP. Based on ethers.js
---
+++ index.ts
@@ -0,0 +1,42 @@
+import * as ethers from 'https://esm.sh/ethers@5.7.2'
+import { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts"
+
+const humanReadableToJson = (data: string[]) => {
+  const abi = new ethers.utils.Interface(data)
+
+  return abi.format(ethers.utils.FormatTypes.json)
+}
+
+const jsonToHumanReadable = (data: ethers.utils.Fragment[]) => {
+  const abi = new ethers.utils.Interface(data)
+
+  return abi.format(ethers.utils.FormatTypes.full)
+}
+
+// ----
+
+const router = new Router()
+router
+  .get("/", (context) => {
+    context.response.body = "Hello world!"
+  })
+  .post("/to-json", async (context) => {
+    const body = await context.request.body().value
+
+    context.response.body = {
+      abi: humanReadableToJson(body.abi)
+    }
+  })
+  .post("/from-json", async (context) => {
+    const body = await context.request.body().value
+
+    context.response.body = {
+      abi: jsonToHumanReadable(body.abi)
+    }
+  })
+  
+const app = new Application()
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+await app.listen({ port: 8000 })
